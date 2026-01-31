5cb6012468b38efd64680add117334cec66da010
danilopinho75 <danilopinho11@gmail.com>
2026-01-31 12:06:31 -0300

    Criando e estilizando componentes de dicas e palavras

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import styles from "./app.module.css";
 
 import { Header } from "./components/Header";
+import { Letter } from "./components/Letter";
+import { Tip } from "./components/Tip";
 
 export default function App() {
 
@@ -12,6 +14,15 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Essa é uma dica legal!" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="e" />
+          <Letter value="a" />
+          <Letter value="c" />
+          <Letter value="t" />
+        </div>
       </main>
     </div>
   );
