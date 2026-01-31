e8f9f192b4cee58d5817fffe0fbaf11d1efbf014
danilopinho75 <danilopinho11@gmail.com>
2026-01-31 12:21:55 -0300

    Cria componente de input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,10 @@
+import type { ComponentProps } from "react";
+import styles from "./styles.module.css";
+
+type Props = ComponentProps<"input">
+
+export function Input({ ...rest }: Props) {
+  return (
+    <input type="text" className={styles.input} {...rest} />
+  )
+}
